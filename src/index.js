88f2414c26fc22c08526adc2ccb36a59e383e7d3
xiaoyufei <915724526@qq.com>
2019-08-26 11:27:12 +0800

    feat : add note

--- src/index.js
+++ src/index.js
@@ -6,41 +6,37 @@ export default function (babel) {
     const { types: t } = babel;
     let imports = []
     return {
-        name: 'ast-transform', // not required
+        name: 'elfin',
         visitor: {
-            Program(path, state) {
+            Program(path) {
                 const {file} = path.hub
+                // 清空缓存
                 imports = []
-
-                // Clear tracked method imports.
-                let isModule = false
-
-                for (const node of file.ast.program.body) {
-                    if (isModuleDeclaration(node)) {
-                        isModule = true
-                        break
-                    }
-                }
+                // 在body下是否有import模块
+                const isModule = file.ast.program.body.some(node => {
+                    return isModuleDeclaration(node)
+                })
 
                 if (isModule) {
+                    // 收集import依赖
                     file.path.traverse({
                         ImportDeclaration: {
                             exit(path) {
                                 const {node} = path
+                                // 是否是lodash依赖
                                 if (isLodash(node.source.value)) return
-
+                                // 依赖信息收集
                                 const imported = []
                                 const specifiers = []
-
                                 imports.push({
-                                    source: node.source.value,
-                                    imported,
-                                    specifiers,
+                                    source: node.source.value, // 来源
+                                    imported, // 导入名
+                                    specifiers, // 导入形式
                                 })
 
                                 for (const specifier of path.get('specifiers')) {
                                     const local = specifier.node.local.name
-
+                                    // import _ from 'lodash'
                                     if (specifier.isImportDefaultSpecifier()) {
                                         imported.push('default')
                                         specifiers.push({
@@ -49,7 +45,7 @@ export default function (babel) {
                                             local,
                                         })
                                     }
-
+                                    // import {cloneDeep} from 'lodash'
                                     if (specifier.isImportSpecifier()) {
                                         const importedName = specifier.node.imported.name
                                         imported.push(importedName)
@@ -59,7 +55,7 @@ export default function (babel) {
                                             local
                                         })
                                     }
-
+                                    // import * as _ from 'lodash'
                                     if (specifier.isImportNamespaceSpecifier()) {
                                         imported.push('*')
                                         specifiers.push({
@@ -80,25 +76,29 @@ export default function (babel) {
                     const object = node.callee.object
                     // 是以属性调用的形式使用 glodash.cloneDeep()
                     if (object && object.name !== 'glodash') return
-                    // 是否自定义了 glodash
-                    if (file.scope.getBinding('glodash') || !node.callee.property) return
-                    const property = node.callee.property.name
+                    const property = node.callee.property
+                    // 是否自定义了 glodash 变量
+                    if (file.scope.getBinding('glodash') || !property) return
+                    const propertyName = property.name
+                    // 如果没有import项，直接导入
                     if (imports.length === 0) {
-                        addDefault(path, `lodash/${property}`, {nameHint: property})
+                        addDefault(path, `lodash/${propertyName}`, {nameHint: propertyName})
                         path.replaceWith(
-                            t.callExpression(t.identifier(`_${property}`), [])
+                            t.callExpression(t.identifier(`_${propertyName}`), [])
                         )
                         return
                     }
+                    // 如果有import项，需要去重
                     for (let module of imports) {
                         const specifier = module.specifier || []
+                        // 是否已经定义了该propertyName的import项
                         const isDefine = specifier.find(e => {
-                            return e.kind !== 'default' && e.local === property
+                            return e.kind !== 'default' && e.local === propertyName
                         })
                         if (isDefine) continue;
-                        addDefault(path, `lodash/${property}`, {nameHint: property})
+                        addDefault(path, `lodash/${propertyName}`, {nameHint: propertyName})
                         path.replaceWith(
-                            t.callExpression(t.identifier(`_${property}`), [])
+                            t.callExpression(t.identifier(`_${propertyName}`), [])
                         )
                     }
                 },
